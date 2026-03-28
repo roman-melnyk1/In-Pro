98c4d35ddfab7dd83e497b94f70d376e0f122367
iyrd <chilromko@gmail.com>
2026-03-28 21:20:21 +0100

    added section obeserver

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const burgerBtn = document.getElementById('burgerToggle');
 const navMenu = document.getElementById('navMenu');
-  
+const sections = document.querySelectorAll('section[id]');
+const navLinks = document.querySelectorAll('.nav-link');
+
 burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('active');
     navMenu.classList.toggle('active');
@@ -18,4 +20,33 @@ document.querySelectorAll('.nav-link , .btn-primary').forEach(link => {
         navMenu.classList.remove('active');
         document.body.style.overflow = '';
     });
+});
+
+
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5 
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const currentId = entry.target.getAttribute('id');
+
+            navLinks.forEach(link => {
+
+                link.classList.remove('active');
+                
+                if (link.getAttribute('href') === `#${currentId}`) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+}, observerOptions);
+
+sections.forEach(section => {
+    observer.observe(section);
 });
